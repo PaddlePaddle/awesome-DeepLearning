4e7e291fabedf0566b20f6dd9d5ef38bd880b6fe
lvjian0706 <657300142@qq.com>
2021-02-22 14:54:50 +0800

    add chapter-3 and chapter-4

---
+++ chapter-3-Computer_Vision/3-1-3_Average_filtering.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+ **案例3——图像均值模糊**
+ 
+ 另外一种比较常见的卷积核（5\*5的卷积核中每个值均为1）是用当前像素跟它邻域内的像素取平均，这样可以使图像上噪声比较大的点变得更平滑，如下代码所示：
+'''
+
+import paddle
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from paddle.nn import Conv2D
+from paddle.nn.initializer import Assign
+# 读入图片并转成numpy.ndarray
+# 换成灰度图
+img = Image.open('000000355610.jpg').convert('L')
+img = np.array(img)
+
+# 创建初始化参数
+w = np.ones([1, 1, 5, 5], dtype = 'float32')/25
+conv = Conv2D(in_channels=1, out_channels=1, kernel_size=[5, 5], 
+        weight_attr=paddle.ParamAttr(
+         initializer=Assign(value=w)))
+x = img.astype('float32')
+x = x.reshape(1,1,img.shape[0], img.shape[1])
+x = paddle.to_tensor(x)
+y = conv(x)
+out = y.numpy()
+
+plt.figure(figsize=(20, 12))
+f = plt.subplot(121)
+f.set_title('input image')
+plt.imshow(img, cmap='gray')
+
+f = plt.subplot(122)
+f.set_title('output feature map')
+out = out.squeeze()
+plt.imshow(out, cmap='gray')
+
+plt.show()
