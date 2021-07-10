7bf044cad7647dba2098aaefb108a4d2a9921cf0
liyanachung <liyanachung@foxmail.com>
2021-07-10 12:47:04 +0800

    Implemented a housing price prediction case

---
+++ examples/housing-python+numpy.py
@@ -0,0 +1,175 @@
+# In[1]:
+
+
+# 导入需要用到的package
+import numpy as np
+import json
+# 读入数据
+datafile = '/home/aistudio/data/data7804/housing.data'
+data = np.fromfile(datafile, sep=' ')
+data
+
+
+# In[2]:
+# 读入之后的数据被转化成1维array，其中array的第0-13项是第一条数据，第14-27项是第二条数据，以此类推.... 
+# 这里对原始数据做reshape，变成N x 14的形式
+feature_names = [ 'CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE','DIS', 
+                 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV' ]
+feature_num = len(feature_names)
+data = data.reshape([data.shape[0] // feature_num, feature_num])
+
+
+# In[3]:
+
+# 查看数据
+x = data[0]
+print(x.shape)
+print(x)
+
+
+# In[4]:
+
+
+ratio = 0.8
+offset = int(data.shape[0] * ratio)
+training_data = data[:offset]
+training_data.shape
+
+
+# In[5]:
+
+
+# 计算train数据集的最大值，最小值，平均值
+maximums, minimums, avgs =                      training_data.max(axis=0),                      training_data.min(axis=0),      training_data.sum(axis=0) / training_data.shape[0]
+# 对数据进行归一化处理
+for i in range(feature_num):
+    #print(maximums[i], minimums[i], avgs[i])
+    data[:, i] = (data[:, i] - minimums[i]) / (maximums[i] - minimums[i])
+
+
+# In[6]:
+
+
+def load_data():
+    # 从文件导入数据
+    datafile = '/home/aistudio/data/data7804/housing.data'
+    data = np.fromfile(datafile, sep=' ')
+
+    # 每条数据包括14项，其中前面13项是影响因素，第14项是相应的房屋价格中位数
+    feature_names = [ 'CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE',                       'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV' ]
+    feature_num = len(feature_names)
+
+    # 将原始数据进行Reshape，变成[N, 14]这样的形状
+    data = data.reshape([data.shape[0] // feature_num, feature_num])
+
+    # 将原数据集拆分成训练集和测试集
+    # 这里使用80%的数据做训练，20%的数据做测试
+    # 测试集和训练集必须是没有交集的
+    ratio = 0.8
+    offset = int(data.shape[0] * ratio)
+    training_data = data[:offset]
+
+    # 计算训练集的最大值，最小值，平均值
+    maximums, minimums, avgs = training_data.max(axis=0), training_data.min(axis=0),                                  training_data.sum(axis=0) / training_data.shape[0]
+
+    # 对数据进行归一化处理
+    for i in range(feature_num):
+        #print(maximums[i], minimums[i], avgs[i])
+        data[:, i] = (data[:, i] - minimums[i]) / (maximums[i] - minimums[i])
+
+    # 训练集和测试集的划分比例
+    training_data = data[:offset]
+    test_data = data[offset:]
+    return training_data, test_data
+
+
+# In[7]:
+
+
+# 获取数据
+training_data, test_data = load_data()
+x = training_data[:, :-1]
+y = training_data[:, -1:]
+
+
+# In[8]:
+
+
+# 查看数据
+print(x[0])
+print(y[0])
+
+
+# In[9]:
+
+
+w = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, -0.1, -0.2, -0.3, -0.4, 0.0]
+w = np.array(w).reshape([13, 1])
+
+
+# In[12]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+class Network(object):
+    def __init__(self, num_of_weights):
+        # 随机产生w的初始值
+        # 为了保持程序每次运行结果的一致性，此处设置固定的随机数种子
+        np.random.seed(0)
+        self.w = np.random.randn(num_of_weights, 1)
+        self.b = 0.
+        
+    def forward(self, x):
+        z = np.dot(x, self.w) + self.b
+        return z
+    
+    def loss(self, z, y):
+        error = z - y
+        num_samples = error.shape[0]
+        cost = error * error
+        cost = np.sum(cost) / num_samples
+        return cost
+    
+    def gradient(self, x, y):
+        z = self.forward(x)
+        gradient_w = (z-y)*x
+        gradient_w = np.mean(gradient_w, axis=0)
+        gradient_w = gradient_w[:, np.newaxis]
+        gradient_b = (z - y)
+        gradient_b = np.mean(gradient_b)        
+        return gradient_w, gradient_b
+    
+    def update(self, gradient_w, gradient_b, eta = 0.01):
+        self.w = self.w - eta * gradient_w
+        self.b = self.b - eta * gradient_b
+        
+    def train(self, x, y, iterations=100, eta=0.01):
+        losses = []
+        for i in range(iterations):
+            z = self.forward(x)
+            L = self.loss(z, y)
+            gradient_w, gradient_b = self.gradient(x, y)
+            self.update(gradient_w, gradient_b, eta)
+            losses.append(L)
+            if (i+1) % 10 == 0:
+                print('iter {}, loss {}'.format(i, L))
+        return losses
+
+# 获取数据
+train_data, test_data = load_data()
+x = train_data[:, :-1]
+y = train_data[:, -1:]
+# 创建网络
+net = Network(13)
+num_iterations=1000
+# 启动训练
+losses = net.train(x,y, iterations=num_iterations, eta=0.01)
+
+# 画出损失函数的变化趋势
+plot_x = np.arange(num_iterations)
+plot_y = np.array(losses)
+plt.plot(plot_x, plot_y)
+plt.show()
+
