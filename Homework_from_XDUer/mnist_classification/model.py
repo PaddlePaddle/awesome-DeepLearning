e9f7dca33e5680ebb22bc72937b2c04edc07c2f9
xiaolin199912 <944270057@qq.com>
2021-07-10 21:40:44 +0800

    xiaolin199912

---
+++ Homework_from_XDUer/mnist_classification/model.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+import paddle
+import paddle.nn.functional as F
+
+class MiniNet(paddle.nn.Layer):
+    def __init__(self):
+        super(MiniNet, self).__init__()
+        self.conv1 = paddle.nn.Conv2D(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2)
+        self.max_pool1 = paddle.nn.MaxPool2D(kernel_size=2,  stride=2)
+        self.conv2 = paddle.nn.Conv2D(in_channels=6, out_channels=16, kernel_size=5, stride=1)
+        self.max_pool2 = paddle.nn.MaxPool2D(kernel_size=2, stride=2)
+        self.fc1 = paddle.nn.Linear(in_features=16*5*5, out_features=120)
+        self.fc2 = paddle.nn.Linear(in_features=120, out_features=84)
+        self.fc3 = paddle.nn.Linear(in_features=84, out_features=10)
+
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.sigmoid(x)
+        x = self.max_pool1(x)
+        x = F.sigmoid(x)
+        x = self.conv2(x)
+        x = self.max_pool2(x)
+        x = paddle.flatten(x, start_axis=1,stop_axis=-1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
+
