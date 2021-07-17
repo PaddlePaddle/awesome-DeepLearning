024b80dbfb8d10f873d3b09cc155831754a00d92
Blddwkb <1498019826@qq.com>
2021-07-17 22:43:43 +0800

    Update demo.py

--- examples/基于yolov5的工业瑕疵检测/demo.py
+++ examples/基于yolov5的工业瑕疵检测/demo.py
@@ -1,4 +1,4 @@
-from v5 import Detector
+from Detector import Detector
 #import imutils
 import cv2
 import os
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     name = 'demo'
 
     det = Detector()
-    #cap = cv2.VideoCapture('./1.mp4')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('demo/a.avi')
+    #cap = cv2.VideoCapture(0)
     fps = cap.get(cv2.CAP_PROP_FPS) #视频平均帧率
     print('FPS : ', fps)
     t = int(1000/fps)
@@ -23,7 +23,7 @@ if __name__ == '__main__':
             x1, y1 = xyxy[0], xyxy[1]
             x2, y2 = xyxy[2], xyxy[3]
             x_center, y_center = int((x1+x2)/2), int((y1+y2)/2)
-            print("在({},{})处有{}缺陷，置信度为{:.2f}".format(x_center, y_center, cls, conf))
+            #print("在({},{})处有{}缺陷，置信度为{:.2f}".format(x_center, y_center, cls, conf))
             
         cv2.imshow(name, result)
         if cv2.waitKey(t) & 0xFF == ord('q') :
