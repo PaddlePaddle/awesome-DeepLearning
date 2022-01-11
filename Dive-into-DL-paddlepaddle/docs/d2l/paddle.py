0e30197c021b74ff2f0f80d389767dac53f93768
tngt <tngt1206@163.com>
2022-01-11 19:20:56 +0800

    #d2l add chap8 and update d2l

--- Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
+++ Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
@@ -472,7 +472,31 @@ class Residual(nn.Layer):
         Y += X
         return F.relu(Y)
 
+"""8.1"""
+class Residual(nn.Layer):
+    def __init__(self, input_channels, num_channels, use_1x1conv=False,
+                 strides=1):
+        super(Residual, self).__init__()
+        self.conv1 = nn.Conv2D(input_channels, num_channels, kernel_size=3,
+                               padding=1, stride=strides)
+        self.conv2 = nn.Conv2D(num_channels, num_channels, kernel_size=3,
+                               padding=1)
+        if use_1x1conv:
+            self.conv3 = nn.Conv2D(input_channels, num_channels,
+                                   kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2D(num_channels)
+        self.bn2 = nn.BatchNorm2D(num_channels)
+        self.relu = nn.ReLU()
 
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return F.relu(Y)
 
 ones = paddle.ones
 zeros = paddle.zeros
