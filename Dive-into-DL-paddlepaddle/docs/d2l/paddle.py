4c367194b6f1d806326573498993ac4df8752df4
nsCosine <weiwu1906@gmail.com>
2022-01-12 13:20:14 +0800

    #d2l add 13.4_bounding-box.md

--- Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
+++ Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
@@ -473,6 +473,27 @@ class Residual(nn.Layer):
         return F.relu(Y)
 
 
+"""13.4"""
+def box_corner_to_center(boxes):
+    """从（左上，右下）转换到（中间，宽度，高度）"""
+    x1, y1, x2, y2 = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    cx = (x1 + x2) / 2
+    cy = (y1 + y2) / 2
+    w = x2 - x1
+    h = y2 - y1
+    boxes = paddle.stack((cx, cy, w, h), axis=-1)
+    return boxes
+
+def box_center_to_corner(boxes):
+    """从（中间，宽度，高度）转换到（左上，右下）"""
+    cx, cy, w, h = boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3]
+    x1 = cx - 0.5 * w
+    y1 = cy - 0.5 * h
+    x2 = cx + 0.5 * w
+    y2 = cy + 0.5 * h
+    boxes = paddle.stack((x1, y1, x2, y2), axis=-1)
+    return boxes
+
 
 ones = paddle.ones
 zeros = paddle.zeros
