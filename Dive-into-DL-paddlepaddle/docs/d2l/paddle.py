04627f6b0244965f8b800abb6bd3fc09e7936078
JIANG-HS <1090272686@qq.com>
2022-01-13 16:33:55 +0800

    add 8.6.md & paddle.py添加8.6内容
    add 8.6.md & paddle.py添加8.6内容

--- Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
+++ Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
@@ -769,6 +769,46 @@ def train_ch8(net, train_iter, vocab, lr, num_epochs, device, use_random_iter=Fa
     print(predict('traveller'))
 
 
+"""8.6"""
+class RNNModel(nn.Layer):   #@save
+    """循环神经网络模型"""
+    def __init__(self, rnn_layer, vocab_size, **kwargs):
+        super(RNNModel, self).__init__(**kwargs)
+        self.rnn = rnn_layer
+        self.vocab_size = vocab_size
+        self.num_hiddens = self.rnn.hidden_size
+        # 如果RNN是双向的（之后将介绍），num_directions应该是2，否则应该是1
+        if self.rnn.num_directions==1:
+            self.num_directions = 1
+            self.linear = nn.Linear(self.num_hiddens, self.vocab_size)
+        else:
+            self.num_directions = 2
+            self.linear = nn.Linear(self.num_hiddens * 2, self.vocab_size)
+
+    def forward(self, inputs, state):
+        X = F.one_hot(inputs.T, self.vocab_size) # paddle相比torch，这里无需再类型转换
+        Y, state = self.rnn(X, state)
+        # 全连接层首先将Y的形状改为(时间步数*批量大小,隐藏单元数)
+        # 它的输出形状是(时间步数*批量大小,词表大小)。
+        output = self.linear(Y.reshape((-1, Y.shape[-1])))
+        return output, state
+
+    def begin_state(self, device, batch_size=1):
+        if not isinstance(self.rnn, nn.LSTM):
+            # nn.GRU以张量作为隐状态
+            return  paddle.to_tensor(paddle.zeros(shape=[self.num_directions * self.rnn.num_layers,
+                                 batch_size, self.num_hiddens]), place=device)
+
+        else:
+            # nn.LSTM以元组作为隐状态
+            return (paddle.to_tensor(paddle.zeros(
+                shape=[self.num_directions * self.rnn.num_layers,
+                batch_size, self.num_hiddens]),place=device),
+                    paddle.to_tensor(paddle.zeros(
+                        shape=[self.num_directions * self.rnn.num_layers,
+                        batch_size, self.num_hiddens]),place=device))
+
+
 ones = paddle.ones
 zeros = paddle.zeros
 tensor = paddle.to_tensor
