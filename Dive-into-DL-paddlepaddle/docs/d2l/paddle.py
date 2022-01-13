b4917374d09ebc589243b82bcaf6f6eeb7417d80
JiehangXie <xiejiehang@foxmail.com>
2022-01-13 16:03:14 +0100

    #d2l add chap 9.5 and update d2l

--- Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
+++ Dive-into-DL-paddlepaddle/docs/d2l/paddle.py
@@ -28,6 +28,7 @@ from PIL import Image
 from paddle import nn
 from paddle.nn import functional as F
 from paddle.vision import transforms
+from paddle.io import TensorDataset, DataLoader
 
 """2.4"""
 def use_svg_display():
@@ -144,10 +145,11 @@ def sgd(params, lr, batch_size):  #@save
         return a
 
 """3.3"""
-def test_load_array(data_arrays, batch_size, is_train=True):
-    """构造一个Paddle数据迭代器。"""
+def load_array(data_arrays, batch_size, is_train=True):
+    """构造一个PyTorch数据迭代器
+    Defined in :numref:`sec_linear_concise`"""
     dataset = TensorDataset(data_arrays)
-    return DataLoader(dataset, batch_size, shuffle=is_train)
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=is_train)
 
 """3.5"""
 def get_fashion_mnist_labels(labels):  #@save
@@ -244,7 +246,7 @@ def train_epoch_ch3(net, train_iter, loss, updater):  #@save
         print('loss:', l.sum().item())
 
         if isinstance(updater, paddle.optimizer.Optimizer):
-            # 使用PyTorch内置的优化器和损失函数
+            # 使用Paddle内置的优化器和损失函数
             updater.clear_grad()
             l.sum().backward()
             updater.step()
@@ -621,6 +623,70 @@ def load_data_time_machine(batch_size, num_steps,
     data_iter = SeqDataLoader(
         batch_size, num_steps, use_random_iter, max_tokens)
     return data_iter, data_iter.vocab
+
+"""9.5"""
+d2l.DATA_HUB['fra-eng'] = (d2l.DATA_URL + 'fra-eng.zip',
+                           '94646ad1522d915e7b0f9296181140edcf86a4f5')
+def read_data_nmt():
+    """载入“英语－法语”数据集"""
+    data_dir = d2l.download_extract('fra-eng')
+    with open(os.path.join(data_dir, 'fra.txt'), 'r',
+             encoding='utf-8') as f:
+        return f.read()
+
+def preprocess_nmt(text):
+    """预处理“英语－法语”数据集"""
+    def no_space(char, prev_char):
+        return char in set(',.!?') and prev_char != ' '
+
+    # 使用空格替换不间断空格
+    # 使用小写字母替换大写字母
+    text = text.replace('\u202f', ' ').replace('\xa0', ' ').lower()
+    # 在单词和标点符号之间插入空格
+    out = [' ' + char if i > 0 and no_space(char, text[i - 1]) else char
+           for i, char in enumerate(text)]
+    return ''.join(out)
+
+def tokenize_nmt(text, num_examples=None):
+    """词元化“英语－法语”数据数据集"""
+    source, target = [], []
+    for i, line in enumerate(text.split('\n')):
+        if num_examples and i > num_examples:
+            break
+        parts = line.split('\t')
+        if len(parts) == 2:
+            source.append(parts[0].split(' '))
+            target.append(parts[1].split(' '))
+    return source, target
+
+def truncate_pad(line, num_steps, padding_token):
+    """截断或填充文本序列"""
+    if len(line) > num_steps:
+        return line[:num_steps]  # 截断
+    return line + [padding_token] * (num_steps - len(line))  # 填充
+
+def build_array_nmt(lines, vocab, num_steps):
+    """将机器翻译的文本序列转换成小批量"""
+    lines = [vocab[l] for l in lines]
+    lines = [l + [vocab['<eos>']] for l in lines]
+    array = paddle.to_tensor([truncate_pad(
+        l, num_steps, vocab['<pad>']) for l in lines])
+    valid_len = (array != vocab['<pad>']).astype(paddle.int32).sum(1)
+    return array, valid_len
+
+def load_data_nmt(batch_size, num_steps, num_examples=600):
+    """返回翻译数据集的迭代器和词表"""
+    text = preprocess_nmt(read_data_nmt())
+    source, target = tokenize_nmt(text, num_examples)
+    src_vocab = d2l.Vocab(source, min_freq=2,
+                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    tgt_vocab = d2l.Vocab(target, min_freq=2,
+                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    src_array, src_valid_len = build_array_nmt(source, src_vocab, num_steps)
+    tgt_array, tgt_valid_len = build_array_nmt(target, tgt_vocab, num_steps)
+    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
+    data_iter = d2l.load_array(data_arrays, batch_size)
+    return data_iter, src_vocab, tgt_vocab
 
 ones = paddle.ones
 zeros = paddle.zeros
